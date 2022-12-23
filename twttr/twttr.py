7e9ac8fd350a7e115174d14b3d1312651049fa85
Elektra-V <vedika.chauhan@outlook.com>
2022-12-23 23:47:35 +0530

    week 2

---
+++ twttr/twttr.py
@@ -0,0 +1,22 @@
+def main():
+  user_input = input("Input: ")
+  print(omit_vowel(user_input))
+
+
+def omit_vowel(msg: str) -> str:
+  msg = msg
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  new_msg = ''
+
+  for letter in msg:
+
+    if letter in vowels:
+      letter = ""
+
+    new_msg += letter
+  
+  return f"Output: {new_msg}"
+
+  
+if __name__ == "__main__":
+  main()
